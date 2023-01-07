04f5d08fe2532568f5f788f1aa79a7b47b3fec6b
Nucs <elibelash@gmail.com>
2023-01-07 08:53:09 +0200

    Removed winforms example

--- source/Loggly.Example/Properties/AssemblyInfoUnique.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Security;
-
-[assembly: AssemblyTitle("Loggly.Example")]
-[assembly: AssemblyDescription("WinForms app that demonstrates client features")]
